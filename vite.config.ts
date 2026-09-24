68357a4c6a92dfbf34849a36f1f507c152dcf411
moha700m <64600035+moha700m@users.noreply.github.com>
2026-09-24 20:32:46 +0300

    chore: make Vite config Vercel-ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
-    outDir: process.env.APPDEPLOY_VITE_OUT_DIR || 'dist',
-    sourcemap:
-      process.env.APPDEPLOY_VITE_SOURCEMAP === 'hidden' ? 'hidden' : false,
-    rollupOptions: {
-      maxParallelFileOps: 128,
-    },
+    outDir: 'dist',
   },
 });
